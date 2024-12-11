9980cb2dfb9c120ba6b809e20254f5357c2d7c49
kalem <154002614+kalemj@users.noreply.github.com>
2024-12-11 02:44:47 +0000

    Add files via upload

---
+++ magic8ball.py
@@ -0,0 +1,42 @@
+import random
+
+random_number = random.randint(1,10)
+
+name = input("What is your name?\n")
+question = input("What is your question?\n")
+
+
+if random_number == 1:
+  answer = "Yes - definitely"
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5:
+  answer = "Ask again later"
+elif random_number == 6:
+  answer = "Better not tell you now"
+elif random_number == 7:
+  answer = "My sources say no"
+elif random_number == 8:
+  answer = "Outlook not so good"
+elif random_number == 9:
+  answer = "Very doubtful"
+elif random_number == 10:
+  answer = "no brav"
+else:
+  answer = "Error"
+
+if question == "":
+  print("There was no question asked.")
+else:
+  if name == "":
+    print(f"Question: {question}")
+    print(f"Magic 8-Ball's answer: {answer}")
+  else:
+    print(f"{name} asks: {question}")
+    print(f"Magic 8-Ball's answer: {answer}")
+  
+
